dd0eb22fb8254e29122e1650ec1655f82e1eb53b
amittras-pal <pal.amittras@gmail.com>
2022-01-06 20:00:00 +0530

    minor fix.

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -1,9 +1,4 @@
-import {
-  Box,
-  Container, Progress,
-  Text,
-  useColorMode
-} from "@chakra-ui/react";
+import { Box, Container, Progress, Text, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import { useApiConfig } from "../../../hooks/configuration.query";
 import { useAllTrendingToday } from "../../../hooks/trending.query";
@@ -46,7 +41,7 @@ function Home() {
         bgPosition={"center"}
         bgRepeat={"no-repeat"}>
         <Box width={{ base: "100%", md: "50%" }} px={4} mb={4}>
-          {colorMode == "dark" ? <HeroWhite /> : <HeroBlack />}
+          {colorMode === "dark" ? <HeroWhite /> : <HeroBlack />}
         </Box>
         <Text textAlign={"center"} fontWeight={"bold"} mb={"8"}>
           Millions of movies, TV shows and people to discover. Explore now.
